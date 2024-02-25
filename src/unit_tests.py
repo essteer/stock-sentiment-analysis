1c9d5f43ccb62f1b15be2bd65d2574fcc281e0f9
essteer <essteer@pm.me>
2024-02-25 10:39:49 +0800

    add unit tests for get_horizon() and get_earnings_dates()

--- src/unit_tests.py
+++ src/unit_tests.py
@@ -135,8 +135,8 @@ class UnitTestsAPI(unittest.TestCase):
         # Set default session
         new_session = requests_cache.CachedSession("yfinance.cache")
         # Set baseline ticker value
-        test_ticker = "AAPL"
-        base_ticker = get_ticker(test_ticker, current_session=new_session)
+        ticker_label = "AAPL"
+        test_ticker = get_ticker(ticker_label, current_session=new_session)
         # Test against valid period values
         test_periods = ["3mo", "6mo", "1y"]
         # Expected durations in days
@@ -144,7 +144,7 @@ class UnitTestsAPI(unittest.TestCase):
         
         for period in test_periods:
             # Get history
-            test_history = get_history(base_ticker, period)
+            test_history = get_history(test_ticker, period)
             # Calculate difference between first and last date
             duration = (test_history.index[-1] - test_history.index[0]).days
             # Confirm difference approximates expected time range
@@ -152,7 +152,102 @@ class UnitTestsAPI(unittest.TestCase):
                 abs(duration - expected_durations[period]) <= 7, 
                 f"Error: date range mismatch for '{period}'"
             )
-
+            self.assertTrue(isinstance(test_history, pd.DataFrame), 
+                             f"Error: Pandas DataFrame not obtained for '{period}'")
+            time.sleep(1)
+    
+    
+    def test_get_horizon(self):
+        # Set default session
+        new_session = requests_cache.CachedSession("yfinance.cache")
+        # Set baseline ticker value
+        ticker_label = "AAPL"
+        test_ticker = get_ticker(ticker_label, current_session=new_session)
+        # Get history
+        test_history = get_history(test_ticker)
+        
+        # Test against ints within valid [0, 12] horizon range
+        test_horizons = [0, 5]
+        for test_horizon in test_horizons:
+            # Get horizon date value
+            test_horizon_date = get_horizon(test_history, test_horizon)
+            # Get end date from test_history
+            test_history_end_date = (test_history.index[-1]).strftime("%Y-%m-%d")
+            # Get difference in months
+            m_diff = abs(int(test_horizon_date[5:7]) - int(test_history_end_date[5:7]))
+            # Get difference in days
+            d_diff = abs(int(test_horizon_date[8:]) - int(test_history_end_date[8:]))
+            days_per_month = 30
+            # Combine month and day differences to get total difference in days
+            total_diff = m_diff * days_per_month + d_diff
+            # Calculate expected difference in days between test_history_end_date and test_horizon_date
+            expected_diff = test_horizon * days_per_month
+            
+            # Confirm difference approximates expected time range
+            self.assertTrue(
+                abs(total_diff - expected_diff) <= 7, 
+                f"Error: invalid horizon for '{test_horizon}'"
+            )
+        
+        # Test against values outside valid [0, 12] horizon range
+        test_horizons = [-2, 15]
+        for test_horizon in test_horizons:
+            # Get horizon date value
+            test_horizon_date = get_horizon(test_history, test_horizon)
+            
+            # Negative horizons should be set to 0
+            if test_horizon < 0:
+                self.assertTrue(test_horizon_date == (test_history.index[-1]).strftime("%Y-%m-%d"), 
+                                f"Error: invalid output for negative horizon value '{test_horizon}'")
+            
+            # Horizons > 12 should be set to 12
+            elif test_horizon > 12:
+                test_history_end_date = (test_history.index[-1]).strftime("%Y-%m-%d")
+                y_diff = int(test_horizon_date[:4]) - int(test_history_end_date[:4])
+                m_diff = int(test_horizon_date[5:7]) - int(test_history_end_date[5:7])
+                d_diff = int(test_horizon_date[8:]) - int(test_history_end_date[8:])
+                # Confirm horizon date is approximately 1 year ahead of test_history_end_date
+                self.assertTrue(y_diff == 1, 
+                                f"Error: invalid output for > 12 month horizon value '{test_horizon}'")
+                self.assertTrue(m_diff == 0, 
+                                f"Error: invalid output for > 12 month horizon value '{test_horizon}'")
+                self.assertTrue(abs(d_diff) <= 5, 
+                                f"Error: invalid output for > 12 month horizon value '{test_horizon}'")
+    
+    
+    def test_get_earnings_dates(self):
+        # Set default session
+        new_session = requests_cache.CachedSession("yfinance.cache")
+        
+        # Test stock with earnings dates
+        ticker_label = "AAPL"
+        test_ticker = get_ticker(ticker_label, current_session=new_session)
+        # Get history
+        test_history = get_history(test_ticker, period="1y", interval="1wk")
+        # Get horizon date value
+        test_horizon = get_horizon(test_history, horizon_months=3)
+        # Get earnings dates
+        test_earnings = get_earnings_dates(test_ticker, test_history, test_horizon)
+        
+        self.assertTrue(isinstance(test_earnings, list), 
+                        f"Error: invalid object type returned by get_earnings_dates for '{ticker_label}'")
+        self.assertTrue(isinstance(test_earnings[0], str), 
+                        f"Error: invalid element type in earnings_dates list for '{ticker_label}'")
+        
+        # Test for security with no earnings dates
+        ticker_label = "SPY"
+        test_ticker = get_ticker(ticker_label, current_session=new_session)
+        # Get history
+        test_history = get_history(test_ticker, period="1y", interval="1wk")
+        # Get horizon date value
+        test_horizon = get_horizon(test_history, horizon_months=3)
+        # Get earnings dates
+        test_earnings = get_earnings_dates(test_ticker, test_history, test_horizon)
+        
+        self.assertTrue(isinstance(test_earnings, list), 
+                        f"Error: invalid object type returned by get_earnings_dates for '{ticker_label}'")
+        self.assertTrue(test_earnings == [], 
+                        f"Error: earnings_dates list not empty for '{ticker_label}'")
 
 
 if __name__ == "__main__":

f451f6b0124765d1f40f206930ba1e7033210744
ndkma <nicowork13@gmail.com>
2024-03-12 14:54:54 +0800

    Update streamlit layout via new columns and wide-mode

--- src/app.py
+++ src/app.py
@@ -78,7 +78,7 @@ def plot_candlestick(fig: go.Figure, ticker_code: str, history: pd.DataFrame, ho
     ticker_label = f"{name} ({ticker_code.upper()}) {interval_label}"
     # Update layout
     fig.update_layout(title_text=f"{ticker_label} Market Data <br>{date_range_label}",
-                      xaxis_rangeslider_visible=False, width=705, height=375)
+                      xaxis_rangeslider_visible=False, width=1080, height=560)
 
     # Add earnings dates as vertical lines
     if earnings_dates != []:
@@ -174,7 +174,7 @@ def handle_plots(sent_df: pd.DataFrame | None, raw_tick: str, tick_history: pd.D
     # fig.show()
 
     # STREAMLIT CHANGE
-    st.plotly_chart(fig, use_container_width=False)
+    st.plotly_chart(fig, use_container_width=True)
 
 
 def run_once(raw_ticker: str, raw_period: str="3mo", raw_interval: str="1d", show_plots=False) -> None:
@@ -226,47 +226,56 @@ def run_once(raw_ticker: str, raw_period: str="3mo", raw_interval: str="1d", sho
 # ===============================================================
 # Streamlit
 # ===============================================================
+        
+# Specify wide layout
+st.set_page_config(layout = 'wide')       
 
-st.title("Stock Price and Market Sentiment Analysis")
-st.subheader(
-    "Explore historical price data and general stock sentiment derived from news headlines in the last 30 days."
-    )
+col11, col12 = st.columns([1, 3])
+with col12:
+    st.title("Stock Price and Market Sentiment Analysis")
+
+col13, col14 = st.columns([1, 6])
+with col14:
+    st.subheader(
+        "Explore historical price data and general stock sentiment derived from news headlines in the last 30 days."
+        )
 
 # Create columns for input and dropdowns
-col1, col2, col3 = st.columns(3)
+col_inp_1, col_inp_2, col_inp_3, col_inp_4, col_inp_5 = st.columns(5)
 
-with col1:
+with col_inp_2:
     ticker_input = st.text_input(label = "Ticker:", value = "NVDA", max_chars = None)
 
-with col2:
+with col_inp_3:
     period_dd = st.selectbox(label = "Period:", options = ["Last 3 months", "Last 6 months", "Last 12 months"], index = 2)
 
-with col3: 
+with col_inp_4: 
     interval_dd = st.selectbox(label = "Interval:", options = ["1 day", "1 week"], index = 0)
 
 # Create columns for run button and process messages
-col4, col5, col6 = st.columns(3)
-col7, col8, col9 = st.columns(3)
+col_but_1, col_but_2, col_but_3 = st.columns([2, 6, 2])
+col_link_1, col_link_2, col_link_3, col_link_4, col_link_5 = st.columns(5)
+col_info_1, col_info_2, col_info_3, col_info_4, col_info_5 = st.columns(5)
 
-with col4:
+with col_but_2:
     if st.button("Generate plot"):
         sl_ticker = ticker_input
         sl_period = "3mo" if period_dd == "Last 3 months" else "6mo" if period_dd == "Last 6 months" else "1y"
         sl_interval = "1d" if interval_dd == "1 day" else "1wk"
 
-        with col5:
+        with col_info_2:
             working_text = st.text("Generating plot...")
 
         # Plot the graph
         run_once(sl_ticker, sl_period, sl_interval, True)
 
         # Remove text from column 5
-        with col5:
+        with col_info_2:
             working_text.empty()
 
-        with col7:
+        with col_link_2:
             st.link_button("GitHub.com/essteer :arrow_right:", "https://github.com/essteer")
 
-        with col8:    
+        with col_link_3:    
             st.link_button("GitHub.com/ndkma :arrow_right:", "https://github.com/ndkma")
 
